9ae21a7ed1f46ada44488c35a0b742937f5bb4fa
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2023-08-19 13:10:57 +0600

    Create Category done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { UserRoutes } from "../modules/user/user.route";
+import { CategoryRoutes } from "../modules/category/category.route";
 
 const routes = express.Router();
 
@@ -7,6 +8,10 @@ const moduleRoutes = [
   {
     path: "/user",
     route: UserRoutes,
+  },
+  {
+    path: "/category",
+    route: CategoryRoutes,
   },
 ];
 
