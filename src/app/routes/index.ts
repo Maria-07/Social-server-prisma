3fee563300c375692daab36f4269a8cc60ec356c
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2023-08-19 12:23:40 +0600

    prisma project initalisation

---
+++ src/app/routes/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import { UserRoutes } from "../modules/user/user.route";
+
+const routes = express.Router();
+
+const moduleRoutes = [
+  {
+    path: "/user",
+    route: UserRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => {
+  routes.use(route.path, route.route);
+});
+
+export default routes;
